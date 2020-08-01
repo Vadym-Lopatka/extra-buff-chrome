4c367cb09dcda8d29e77c3852c13d4010dd5005a
vlopatka <vlopatka@overonix.com>
2020-08-01 08:45:05 +0300

    [01_v] - can add one(last copied) value to the store

--- background.js
+++ background.js
@@ -1,75 +1,46 @@
-const countMe = info => {
-    const selectedText = info.selectionText;
-    const totalWords = selectedText.trim().split(' ');
+chrome.commands.onCommand.addListener(function (cmd) {
 
-    // Chrome
-    new Notification("hello", {
-        icon: 'icon.png',
-        body: `Total words ${totalWords.length}\nTotal Characters: ${selectedText.length}`
-    });
-}
-
-// Chrome
-chrome.contextMenus.create({
-    "title": "Word Count",
-    "id": "count-me",
-    "onclick": countMe,
-    "contexts": ["all"]
-}, function () {
-    // Do what all you need here when created
-    console.log("Context Menu 2 Created");
+    if (cmd == "Ctrl+C") {
+        addToBuffer()
+    } else if (cmd == "Ctrl+Shift+V") {
+        showBuffer(function (value) {
+            console.log("Buffer: " + value['my-buff'])
+        })
+    }
 });
 
+function showBuffer(callback) {
+    chrome.storage.sync.get('my-buff', callback);
+}
 
-/* The function that finds and returns the selected text */
+/*==========================================================================*/
+
 var funcToInject = function () {
-    var selection = window.getSelection();
+    const selection = window.getSelection();
     return (selection.rangeCount > 0) ? selection.toString() : '';
 };
 
-/* This line converts the above function to string
- * (and makes sure it will be called instantly) */
 var jsCodeStr = ';(' + funcToInject + ')();';
 
-chrome.commands.onCommand.addListener(function (cmd) {
-    /* Inject the code into all frames of the active tab */
-    chrome.tabs.executeScript({
-        code: jsCodeStr,
-        allFrames: true   //  <-- inject into all frames, as the selection
-                          //      might be in an iframe, not the main page
-    }, function (selectedTextPerFrame) {
-        if (chrome.runtime.lastError) {
-            /* Report any error */
-            alert('ERROR:\n' + chrome.runtime.lastError.message);
-        } else if ((selectedTextPerFrame.length > 0)
-            && (typeof (selectedTextPerFrame[0]) === 'string')) {
-            /* The results are as expected */
-            alert('Selected text: ' + selectedTextPerFrame[0]);
+function addToBuffer() {
+    chrome.tabs.executeScript(
+        {code: jsCodeStr, allFrames: true}, function (selectedTextPerFrame) {
+
+            if (chrome.runtime.lastError) {
+                alert('ERROR:\n' + chrome.runtime.lastError.message);
+
+            } else if ((selectedTextPerFrame.length > 0) && (typeof (selectedTextPerFrame[0]) === 'string')) {
+
+                const theValue = selectedTextPerFrame[0]
+
+                chrome.storage.sync.set({'my-buff': theValue}, function () {
+                        console.log('Value is set to: ' + theValue);
+                    }
+                );
+
+            }
         }
-    });
-});
+    );
+}
 
-/*chrome.commands.onCommand.addListener(function(command) {
-
-    chrome.tabs.query({
-        active: true,
-        lastFocusedWindow: true
-    }, function (tabs) {
-        const myTabUrl = tabs[0].url;
-        console.log('Command:', command + ': ' + myTabUrl);
-    });
-
-    //
-    //
-    // var selectedText = "before";
-    //
-    // chrome.tabs.executeScript( {
-    //     code: "window.getSelection().toString();"
-    // }, function(selection) {
-    //     selectedText = selection[0];
-    // });
-    //
-    // console.log('Command:', command + ': ' + selectedText);
-
-});*/
 
